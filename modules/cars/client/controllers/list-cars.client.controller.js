873dcec291535eaf4c2075940e685c1d4a5623f4
DenBo <denis_bozovic@t-2.net>
2018-04-15 19:38:45 +0200

    If car is rented rent button is disabled

--- modules/cars/client/controllers/list-cars.client.controller.js
+++ modules/cars/client/controllers/list-cars.client.controller.js
@@ -5,12 +5,35 @@
     .module('cars')
     .controller('CarsListController', CarsListController);
 
-  CarsListController.$inject = ['CarsService'];
+  CarsListController.$inject = ['$scope', 'CarsService', 'ActiveRentsService'];
 
-  function CarsListController(CarsService) {
+  function CarsListController($scope, CarsService, ActiveRentsService) {
     var vm = this;
 
-    vm.cars = CarsService.query();
+    vm.cars = {};
+
+    display();
+
+    function display() {
+      return Promise.all([
+        CarsService.query().$promise,
+        ActiveRentsService.query().$promise
+      ])
+      .then(function (values) {
+        vm.cars = values[0];
+        var activeRents = values[1];
+        console.log(values[0]);
+        angular.forEach(vm.cars, function (car) {
+          car.rented = false;
+          angular.forEach(activeRents, function (activeRent) {
+            if (car._id === activeRent.car) {
+              car.rented = true;
+            }
+          });
+        });
+        $scope.$apply();
+      });
+    }
 
     function convertDate(date) {
       return new Date(date);
